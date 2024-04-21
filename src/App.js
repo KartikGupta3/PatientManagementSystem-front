1a73a8d4b7c51647f4395ead46b011ccd4d866af
Kartik Gupta <kkartikgupta30@gmail.com>
2024-04-21 16:35:37 +0530

    routing of pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Appointment from "./components/Appointments/Appointment";
+import Dashboard from "./components/Dashboard/Dashboard";
+import DownloadReport from "./components/DownloadReport/DownloadReports";
+import LoginPage from "./components/LoginPage/Login";
+import ProfilePage from "./components/ProfilePage/Profile";
+import Treatment from "./components/Treatment/SetTreatment";
 
-function App() {
+const App = () => {
   return (
-    <div>Welcome to Patient Management System</div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/appointment" component={Appointment} />
+          <Route path="/treatment" component={Treatment} />
+          <Route path="/download-report" component={DownloadReport} />
+          <Route component={LoginPage} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
